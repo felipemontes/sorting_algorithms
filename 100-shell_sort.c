3e5e12a93f4b5b9b9f04e6f5bcfd9991b6bf4658
Felipe <felipe_londono1@hotmail.com>
2020-02-05 18:07:54 +0000

    [ADD]: shell-sort algorithm

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * shell_sort - function to sort using shell-sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 4;
+	size_t i, j;
+	int tmp;
+
+	if (size < 2)
+		return;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i += 1)
+		{
+			tmp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
+}
